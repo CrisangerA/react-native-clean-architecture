4f1fbd542d642460ce6f55a9ab348bcff17d1a1b
Cristian Alejandro <crisanger@Cristians-MacBook-Pro.local>
2025-06-04 16:40:32 -0500

    feat(firebase): Integrate Firebase in iOS and Android
    - Added Firebase configuration in AppDelegate.swift.
    - Updated Info.plist to include URL schemes for Firebase.

--- ios/ReactNativeCleanArchitecture/AppDelegate.swift
+++ ios/ReactNativeCleanArchitecture/AppDelegate.swift
@@ -3,9 +3,12 @@ import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
 
+import Firebase
+
 @main
 class AppDelegate: RCTAppDelegate {
   override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
     self.moduleName = "ReactNativeCleanArchitecture"
     self.dependencyProvider = RCTAppDependencyProvider()
 
